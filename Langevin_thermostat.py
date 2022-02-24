f6632b6efd1dd1fb345d7a261116cc41b958edd4
Youri Mancia <yourimancia@gmail.com>
2022-02-24 14:41:09 +0100

    [Error Handling] Added a simulation logging functionality.
    [Error Handling] Added a simulation logging functionality.

--- Langevin_thermostat.py
+++ Langevin_thermostat.py
@@ -9,22 +9,6 @@ import numpy as np
 import matplotlib.pylab as plt
 import utils
 
-def inputParams():
-
-    params = {'natoms': 500, 'temp' : 300, 'mass' : 0.001, 'radius' : 120e-12, 'relax' : 1e-13,
-              'timestep': 1e-15, 'maxsteps' : 3000, 'outputfreq' : 10, 'outputfile': 'tray-Langevin-thermo.dump'}
-    with open("config.txt") as file:
-        for line in file:
-            values = line.split()
-            (key, value) = values[0], values[1]
-            if key in params.keys():
-                params[key] = value
-            if key == 'box':
-                box = ((float(values[1]),float(values[2])), (float(values[3]),float(values[4])), 
-                (float(values[5]),float(values[6])))
-                params['box'] = box
-        return params
-
 def Integration(positions, velocities, forces, mass, dt):
     positions += velocities * dt
     velocities += forces * dt / mass
@@ -48,32 +32,41 @@ def computeInstTemp(velocities, mass, natoms, ndim):
     return temp
 
 def run(**args):
-    natoms, temp, mass, radius = int(args['natoms']), float(args['temp']), float(args['mass']), float(args['radius'])
-    dt, steps, freq, box, relax = float(args['timestep']), int(args['maxsteps']), args['outputfreq'],args['box'],float(args['relax'])
-    outputFile = args['outputfile']
-    output = []
-    positions = np.random.rand(natoms, ndim)
-    velocities = np.random.rand(natoms, ndim)
-    mass = mass/avogadro
-    for i in range(ndim):
-        positions[:,i] *= box[i][0] + (box[i][1] - box[i][0])
+    try:
+        utils.log('---STARTING SIMULATION---')
+        nsteps = 0
+        natoms, temp, mass, radius = int(args['natoms']), float(args['temp']), float(args['mass']), float(args['radius'])
+        dt, steps, freq, box, relax = float(args['timestep']), int(args['maxsteps']), int(args['outputfreq']),args['box'],float(args['relax'])
+        outputFile = args['outputfile']
+        output = []
+        positions = np.random.rand(natoms, ndim)
+        velocities = np.random.rand(natoms, ndim)
+        mass = mass/avogadro
+        for i in range(ndim):
+            positions[:,i] *= box[i][0] + (box[i][1] - box[i][0])
 
-    nsteps = 0
-    open(outputFile, 'w')
-    while (nsteps < steps):
-        nsteps += 1
-        forces = CalculateForces(positions, velocities, relax, mass, temp, dt, natoms, ndim)
-        Integration(positions, velocities, forces, mass, dt)
-        CheckWall(positions, velocities, box, ndim, natoms)
-        output.append([dt*nsteps, computeInstTemp(velocities, mass, natoms, ndim)])
-        if nsteps%freq == 0:
-            utils.writeOutput(outputFile, natoms, dt, box, positions, velocities, np.ones(natoms)*radius)
+        
+        while (nsteps < steps):
+            nsteps += 1
+            forces = CalculateForces(positions, velocities, relax, mass, temp, dt, natoms, ndim)
+            Integration(positions, velocities, forces, mass, dt)
+            CheckWall(positions, velocities, box, ndim, natoms)
+            output.append([dt*nsteps, computeInstTemp(velocities, mass, natoms, ndim)])
+            if nsteps%freq == 0:
+                utils.writeOutput(outputFile, natoms, dt, box, positions, velocities, np.ones(natoms)*radius)
+                utils.log(f'INFO\tExporting output to {outputFile} at step {nsteps}.')
+    except Exception as err:
+        error = str(err)
+        utils.log(f'An error occurred while the simulation was running at step {nsteps}: {error}')
+    utils.log('---ENDING SIMULATION---')
     return np.array(output)  
           
 avogadro = 6.02214086e23
 boltzmann = 1.38064852e-23
 ndim = 3
-params = inputParams()
+utils.ClearLog()
+params = utils.inputParams()
+utils.ClearOutput(params['outputfile'])
 output = run(**params)
 plt.plot(output[:,0]*1e12, output[:,1])
 plt.xlabel('Time (ps)')
--- utils.py
+++ utils.py
@@ -6,6 +6,61 @@ Created on Wed Feb 23 17:09:27 2022
 """
 
 import numpy as np
+import datetime
+
+def inputParams():
+    try:
+        log('---STARTING PARAMETERS LOADING---')
+
+        params = {'natoms': 500, 
+                  'temp' : 300, 
+                  'mass' : 0.001, 
+                  'radius' : 120e-12, 
+                  'relax' : 1e-13,
+                  'timestep': 1e-15, 
+                  'maxsteps' : 3000, 
+                  'outputfreq' : 10, 
+                  'outputfile': 'tray-Langevin-thermo.dump', 
+                  'box' : ((0, 1e-8), (0, 1e-8), (0, 1e-8))}
+    
+        defaultParams = params.copy()
+        with open("config.txt",'r') as file:
+            for line in file:
+                values = line.split()
+                (key, value) = values[0], values[1]
+                if key in params.keys() and key != 'box':
+                    log(f'INFO\tSetting the parameter {key} = {value}.')
+                    params[key] = value
+                    defaultParams.pop(key)
+            
+                elif key == 'box':
+                    box = ((float(values[1]),float(values[2])), (float(values[3]),float(values[4])), 
+                           (float(values[5]),float(values[6])))
+                    params['box'] = box
+                    log(f'INFO\tSetting the parameter box = {box}.')
+            
+                else:
+                    log(f'WARNING\tNo parameter name found for {key} - skipping it.')
+        for key in defaultParams.keys():
+            log(f'INFO\tSetting {key} to default value.')
+            
+    except Exception as err:
+        error = str(err)
+        log(f'An error occurred while loading the input parameters: {error}')
+    finally:
+        log('---ENDING PARAMETERS LOADING---')
+        return params
+    
+def ClearOutput(filename):
+    open(filename, 'w')
+
+def ClearLog():
+    logFile = 'Langevin-simulation-log.txt'
+    open(logFile, 'w')
+    
+def log(string):
+    with open('Langevin-simulation-log.txt', 'a') as fp:
+        fp.write(str(datetime.datetime.now()) + '\t' + string + '\n')
 
 def writeOutput(filename, natoms, timestep, box, positions, velocities, radius):
     
