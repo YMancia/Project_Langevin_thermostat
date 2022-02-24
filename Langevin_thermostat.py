44a86cd3f8dd6a4d12fc8fc3fc055170d97cc3a2
Youri Mancia <yourimancia@gmail.com>
2022-02-24 12:53:07 +0100

    [main changes] implemented file input for parameters
    [main changes] implemented file input for parameters

--- Langevin_thermostat.py
+++ Langevin_thermostat.py
@@ -9,8 +9,23 @@ import numpy as np
 import matplotlib.pylab as plt
 import utils
 
+def inputParams():
 
-def Verlet(positions, velocities, forces, mass, dt):
+    params = {'natoms': 500, 'temp' : 300, 'mass' : 0.001, 'radius' : 120e-12, 'relax' : 1e-13,
+              'timestep': 1e-15, 'maxsteps' : 3000, 'outputfreq' : 10, 'outputfile': 'tray-Langevin-thermo.dump'}
+    with open("config.txt") as file:
+        for line in file:
+            values = line.split()
+            (key, value) = values[0], values[1]
+            if key in params.keys():
+                params[key] = value
+            if key == 'box':
+                box = ((float(values[1]),float(values[2])), (float(values[3]),float(values[4])), 
+                (float(values[5]),float(values[6])))
+                params['box'] = box
+        return params
+
+def Integration(positions, velocities, forces, mass, dt):
     positions += velocities * dt
     velocities += forces * dt / mass
 
@@ -33,21 +48,22 @@ def computeInstTemp(velocities, mass, natoms, ndim):
     return temp
 
 def run(**args):
-    natoms, temp, mass, radius = args['natoms'], args['temp'], args['mass'], args['radius']
-    dt, steps, freq, box, relax = args['dt'], args['steps'], args['freq'],args['box'],args['relax']
-    outputFile = args['outputFile']
+    natoms, temp, mass, radius = int(args['natoms']), float(args['temp']), float(args['mass']), float(args['radius'])
+    dt, steps, freq, box, relax = float(args['timestep']), int(args['maxsteps']), args['outputfreq'],args['box'],float(args['relax'])
+    outputFile = args['outputfile']
     output = []
     positions = np.random.rand(natoms, ndim)
     velocities = np.random.rand(natoms, ndim)
     mass = mass/avogadro
     for i in range(ndim):
         positions[:,i] *= box[i][0] + (box[i][1] - box[i][0])
-    
+
     nsteps = 0
+    open(outputFile, 'w')
     while (nsteps < steps):
         nsteps += 1
         forces = CalculateForces(positions, velocities, relax, mass, temp, dt, natoms, ndim)
-        Verlet(positions, velocities, forces, mass, dt)
+        Integration(positions, velocities, forces, mass, dt)
         CheckWall(positions, velocities, box, ndim, natoms)
         output.append([dt*nsteps, computeInstTemp(velocities, mass, natoms, ndim)])
         if nsteps%freq == 0:
@@ -57,18 +73,7 @@ def run(**args):
 avogadro = 6.02214086e23
 boltzmann = 1.38064852e-23
 ndim = 3
-params = {
-    'natoms': 500,
-    'temp': 300,
-    'mass': 0.001,
-    'radius': 120e-12,
-    'relax' : 1e-13,
-    'dt' : 1e-15,
-    'steps' : 3000,
-    'freq' : 10,
-    'box' : ((0,1e-8),(0,1e-8),(0,1e-8)),
-    'outputFile' : 'tray-Langevin-thermo.dump'
-    }
+params = inputParams()
 output = run(**params)
 plt.plot(output[:,0]*1e12, output[:,1])
 plt.xlabel('Time (ps)')
